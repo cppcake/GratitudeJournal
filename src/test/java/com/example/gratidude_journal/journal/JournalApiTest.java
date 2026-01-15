1e9818b19f05cc48172eb374a645cd2707d1e811
Afeef Neiroukh <marblecake@posteo.com>
2026-01-15 12:17:20 +0100

    Expand Journal-API Test

--- src/test/java/com/example/gratidude_journal/journal/JournalApiTest.java
+++ src/test/java/com/example/gratidude_journal/journal/JournalApiTest.java
@@ -1,11 +1,19 @@
 package com.example.gratidude_journal.journal;
 
+import com.example.gratidude_journal.journal.entry.IdDatePairDTO;
 import com.example.gratidude_journal.journal.entry.JournalEntry;
+import com.example.gratidude_journal.journal.entry.JournalEntry.WellBeing;
 import com.example.gratidude_journal.journal.entry.JournalEntryDTO;
 
 import com.example.gratidude_journal.TestcontainersConfiguration;
 
+import org.apache.catalina.connector.Response;
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.time.LocalDate;
+import java.util.Collection;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.resttestclient.autoconfigure.AutoConfigureRestTestClient;
@@ -27,10 +35,11 @@ class JournalApiTest {
 	@Autowired
 	private RestTestClient restTestClient;
 
-	//@Autowired
-	//private JournalService journalService;
+	// @Autowired
+	// private JournalService journalService;
 
-	public JournalApiTest() {}
+	public JournalApiTest() {
+	}
 
 	ResponseSpec requestAddEntry(String userName, JournalEntry.WellBeing wellBeing, String gratefullForToday,
 			String gratefullForTodayDescription, String gratefullForInLife, String gratefullForInLifeDescription) {
@@ -43,16 +52,43 @@ class JournalApiTest {
 	}
 
 	ResponseSpec requestGetEntries(String userName) {
-		return restTestClient.post()
+		return restTestClient.get()
 				.uri("http://localhost:%d/journal/%s".formatted(port, userName))
+				.exchange();
+	}
+
+	ResponseSpec requestGetEntry(Long journalEntryId) {
+		return restTestClient.get()
+				.uri("http://localhost:%d/journal/entry/%d".formatted(port, journalEntryId))
 				.exchange();
 	}
 
 	@Test
 	void addEntry() {
-		requestAddEntry("test1UserNameJournal", JournalEntry.WellBeing.GOOD, "A", "AAA", "B",
-				"BBB")
-				.expectStatus().isCreated();
+		WellBeing wellBeing = JournalEntry.WellBeing.GOOD;
+		String gft = "A";
+		String gft_desc = "AAA";
+		String gfl = "B";
+		String gfl_desc = "BBB";
+		requestAddEntry("test1UserNameJournal", wellBeing, gft, gft_desc, gfl, gfl_desc).expectStatus().isCreated();
+
+		IdDatePairDTO[] entries = requestGetEntries("test1UserNameJournal").expectStatus().isOk()
+				.expectBody(IdDatePairDTO[].class).returnResult().getResponseBody();
+
+		assertNotNull(entries);
+		assertEquals(entries.length, 1);
+		assertNotNull(entries[0].id());
+		assertEquals(entries[0].date(), LocalDate.now());
+
+		requestGetEntry(entries[0].id()).expectStatus().isOk().expectBody(JournalEntry.class)
+				.value(entry -> {
+					org.junit.jupiter.api.Assertions.assertEquals(wellBeing, entry.getWellBeing());
+					org.junit.jupiter.api.Assertions.assertEquals(gft, entry.getGratefullForToday());
+					org.junit.jupiter.api.Assertions.assertEquals(gft_desc, entry.getGratefullForTodayDescription());
+					org.junit.jupiter.api.Assertions.assertEquals(gfl, entry.getGratefullForInLife());
+					org.junit.jupiter.api.Assertions.assertEquals(gfl_desc, entry.getGratefullForInLifeDescription());
+				});
+		;
 	}
 
 	@Test
@@ -67,5 +103,15 @@ class JournalApiTest {
 		requestAddEntry("thisUserDoesNotExist", JournalEntry.WellBeing.GOOD, "A", "AAA", "B",
 				"BBB")
 				.expectStatus().isNotFound();
+	}
+
+	@Test
+	void getEntries() {
+		requestGetEntries("test2UserNameJournal").expectStatus().isOk();
+	}
+
+	@Test
+	void getEntriesForInvalidUser() {
+		requestGetEntries("thisUserDoesNotExist").expectStatus().isNotFound();
 	}
 }
