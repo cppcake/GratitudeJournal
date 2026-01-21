35f59802fa2abe2506e150f75669a3d2d0ce525b
Afeef Neiroukh <marblecake@posteo.com>
2026-01-21 12:37:15 +0100

    docs: Add javadoc for the User entity

--- src/main/java/com/example/gratitude_journal/user/User.java
+++ src/main/java/com/example/gratitude_journal/user/User.java
@@ -15,21 +15,69 @@ import jakarta.persistence.Column;
 
 import com.example.gratitude_journal.journal.Journal;
 
-/*
-    Part of the Persistence Layer for User-API
-*/
+/**
+ * JPA-Entity to represent a user in the GratitudeJournal Service. Part of the
+ * persistence layer of the User-API.
+ * 
+ * Every user is uniquely identified either by its primary key userId or by the
+ * unique userName. Furthermore a user consists of a firstName, lastName and a
+ * journal.
+ * 
+ * The journal and userId are assigned automatically on creation, while the
+ * userName, firstName and lastName must be passed on creation. The userId and
+ * userName are immutable, the firstName and lastName are mutable and the state
+ * of journal is mutable (but the reference is immutable).
+ * 
+ * The DTOs NewUserDTO, ReturnUserDTO and UpdateUserDTO represent parts of the
+ * User. These are used in the User-API to avoid exposing or demanding
+ * irrelevant fields and improve the separation of concerns.
+ * 
+ * @author Afeef Neiroukh
+ */
 @Entity
 public class User {
+    /**
+     * The private primary key of the user.
+     */
     private @Id @GeneratedValue Long userId;
+
+    /**
+     * The private unique name of the user. Used in the API to identify users.
+     */
     @Column(unique = true, updatable = false)
     private String userName;
+
+    /**
+     * The private first name of the user.
+     */
     private String firstName;
+
+    /**
+     * The private last name of the user.
+     */
     private String lastName;
 
+    /**
+     * The journal of the user. Every user has exactly one journal and every journal
+     * belongs to exactly one user (One-To-One Mapping). The journal and its content
+     * are updated/deleted automatically upon updating/deleting the user.
+     */
     @OneToOne(cascade = CascadeType.ALL, orphanRemoval = true)
     @JoinColumn(name = "journal_id", unique = true)
     private Journal journal;
 
+    /**
+     * Static method to validate if a given name is valid. A name is valid if it
+     * fulfills the following conditions:
+     * - It is not null
+     * - Its length is 2 or greater
+     * - It contains no spaces
+     * 
+     * These rules are very relaxed and could be tightened in the future.
+     * 
+     * @param name The name to validate.
+     * @return Returns true if the name is valid and otherwise false.
+     */
     public static boolean validateName(String name) {
         if (name == null)
             return false;
@@ -43,6 +91,19 @@ public class User {
         return true;
     }
 
+    /**
+     * Convenient method to validate three names at once, typically to validate the
+     * userName,
+     * firstName and lastName of a user.
+     * 
+     * @param userName  The first name to validate, usually the user name of the
+     *                  user.
+     * @param firstName The second name to validate, usually the first name of the
+     *                  user.
+     * @param lastName  The third name to validate, usually the last name of the
+     *                  user.
+     * @return True if all three names are valid, otherwise false.
+     */
     public static boolean validateAllNames(String userName, String firstName, String lastName) {
         return validateName(userName) && validateName(firstName) && validateName(lastName);
     }
@@ -51,6 +112,15 @@ public class User {
         this.journal = new Journal();
     }
 
+    /**
+     * Constructs a user and assigns it a new Journal instance.
+     * 
+     * @param userName  The user name of the user to create
+     * @param firstName The first name of the user to create
+     * @param lastName  the last name of the user to create
+     * 
+     * @throws NameInvalidException One or more of the names are invalid.
+     */
     public User(String userName, String firstName, String lastName) {
         this.setUserName(userName);
         this.setFirstName(firstName);
@@ -59,14 +129,33 @@ public class User {
         this.journal = new Journal();
     }
 
+    /**
+     * Returns the userId (primary key) of the user. Because the userId is
+     * immutable, there is no setter for this field.
+     * 
+     * @return The userId (primary key) of the user.
+     */
     public Long getUserId() {
         return userId;
     }
 
+    /**
+     * Returns the userName of the user.
+     * 
+     * @return The userName of the user.
+     */
     public String getUserName() {
         return userName;
     }
 
+    /**
+     * Sets the user name of the user with the userName given. userName is validated
+     * before applying it using the validateName method.
+     * 
+     * @param userName The userName to validate and apply.
+     *
+     * @throws NameInvalidException userName is invalid.
+     */
     private void setUserName(String userName) {
         if (!validateName(userName))
             throw new NameInvalidException(userName);
@@ -74,10 +163,23 @@ public class User {
         this.userName = userName;
     }
 
+    /**
+     * Returns the firstName of the user.
+     * 
+     * @return The firstName of the user.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Sets the first name of the user with the firstName given. firstName is
+     * validated before applying it using the validateName method.
+     * 
+     * @param firstName The firstName to validate and apply.
+     * 
+     * @throws NameInvalidException firstName is invalid.
+     */
     public void setFirstName(String firstName) {
         if (!validateName(firstName))
             throw new NameInvalidException(firstName);
@@ -85,10 +187,23 @@ public class User {
         this.firstName = firstName;
     }
 
+    /**
+     * Returns the lastName of the user.
+     * 
+     * @return The lastName of the user.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Sets the last name of the user with the lastName given. lastName is validated
+     * before applying it using the validateName method.
+     * 
+     * @param lastName The lastName to validate and apply.
+     * 
+     * @throws NameInvalidException lastName is invalid.
+     */
     public void setLastName(String lastName) {
         if (!validateName(lastName))
             throw new NameInvalidException(lastName);
@@ -96,10 +211,22 @@ public class User {
         this.lastName = lastName;
     }
 
+    /**
+     * Returns the journal of the user.
+     * 
+     * @return The journal of the user.
+     */
     public Journal getJournal() {
         return journal;
     }
 
+    /**
+     * Creates and returns a ReturnUserDTO object representing the fields userId,
+     * userName, firstName and lastName of the user.
+     * 
+     * @return A ReturnUserDTO object that includes the fields userId, userName,
+     *         firstName and lastName of the user.
+     */
     public ReturnUserDTO toReturnUserDTO() {
         return new ReturnUserDTO(this.getUserId(), this.getUserName(), this.getFirstName(), this.getLastName());
     }
