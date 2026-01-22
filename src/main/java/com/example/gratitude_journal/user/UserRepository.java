1a625ebca01db70433ec991a878e454c1b4e99a6
Afeef Neiroukh <marblecake@posteo.com>
2026-01-22 14:57:18 +0100

    refactor: remove unused deleteByUserName method from UserRepository and add javadoc

--- src/main/java/com/example/gratitude_journal/user/UserRepository.java
+++ src/main/java/com/example/gratitude_journal/user/UserRepository.java
@@ -3,11 +3,21 @@ package com.example.gratitude_journal.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
-/*
-    Persistence Layer for User-API
-*/
+/**
+ * JpaRepository for the User Entity. Part of the persistence layer of the
+ * User-API.
+ * 
+ * @author Afeef Neiroukh
+ */
 public interface UserRepository extends JpaRepository<User, Long> {
+    /**
+     * Searches the repository for a user matching the provided user name. Returns
+     * an Optional<User> object containing the user matching the provided user name
+     * or an empty Optional<User> object if no user is found.
+     * 
+     * @param userName The user name of the requested user.
+     * @return An Optional<User> object containing the user matching the provided
+     *         user name or an empty Optional<User> object if no user is found.
+     */
     Optional<User> findByUserName(String userName);
-
-    void deleteByUserName(String userName);
 }
